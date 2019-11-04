ab286e374b49321453dbb42bb078cf93f16d2ee7
Ross Stapleton <rstapleton@spartaglobal.com>
2019-11-04 23:08:17 +0000

    first Commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace @throw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThrowErr e = new ThrowErr();
+
+            int[] arr = new int[3];
+            for (int i = 0; i <= arr.Length; i++)
+            {
+                arr[i] = 8;
+                e.ThrowException(i);
+            }
+        }
+    }
+    class ThrowErr
+    {
+        public void ThrowException(int i)
+        {
+            try
+            {
+                Console.WriteLine(++i);
+            }
+            catch (IndexOutOfRangeException err)
+            {
+                Console.WriteLine("an error has occored   :   {0}", err);
+            }
+        }
+    }
+}
